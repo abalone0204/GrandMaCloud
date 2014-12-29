6a464f8beb06f001a9168554379efda628a3cebc
abalone0204 <abalone0204@gmail.com>
2014-12-29 20:16:58 +0800

    add gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sass/assets/javascripts/bootstrap-sprockets
+//= require bootstrap-material-design
 //= require_tree .
